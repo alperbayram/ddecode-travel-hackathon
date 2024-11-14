1b7919605a77d8975da1b6aacc9ab1de5971dbe8
alperbayram <alperbayrm@hotmail.com>
2024-11-14 16:07:19 +0300

    fix: seo

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <h6 className="text-base font-bold text-indigo-600">TIV</h6>
+              <h6 className="text-2xl font-bold text-[#9333ea]">TIV</h6>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
